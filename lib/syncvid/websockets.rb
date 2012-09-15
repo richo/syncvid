c3dabf4f36ba534a0b9da32eb3d6373977c4059f
richo <richo@psych0tik.net>
2012-09-15 21:47:40 +1000

    Refactor websockets around clientpools

--- lib/syncvid/websockets.rb
+++ lib/syncvid/websockets.rb
@@ -1,22 +1,37 @@
 require 'em-websocket'
 module SyncVid
+  $socketpool = Hash.new do |hash, k|
+    hash[k] = Array.new
+  end
+
   WebSocketServer = Proc.new do |ws|
+
     ws.onopen do
-      $proxy.add(ws)
+      pool << ws
       ws.send("buttslol")
       # Do init stuff to get state in sync with anyone else
     end
 
     ws.onclose do
-      $proxy.remove(ws)
+      pool.delete ws
     end
 
     ws.onerror do
-      $proxy.remove(ws)
+      pool.delete ws
     end
 
     ws.onmessage do |msg|
       puts msg
     end
   end
+
+  # Epic hack until we come up with a reasonable mechanism for what to do here
+  def clientkey
+    "buttslol"
+  end
+
+  def pool
+    $socketpool[clientkey]
+  end
+
 end
